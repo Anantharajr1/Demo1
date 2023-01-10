b672fef4e81d7fb1fca52e1e750ae67942009702
AzureAD\AnantharajRamamoorth <anantharaj.ramamoorthy@bounteous.com>
2023-01-10 16:44:00 +0530

    Demo Commit

---
+++ Test/demo.js
@@ -0,0 +1,10 @@
+describe("Ecommerce Test Application validation", function (){
+   
+    it("Verifying the Login page and User details ",async function(){
+ 
+         browser.url("https://www.myntra.com/")
+         await browser.maximizeWindow()
+         await browser.pause(3000)
+         })
+
+    })
